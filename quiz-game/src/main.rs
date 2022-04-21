60c15fce2c66da4758cccd4188fdd1858727af00
Yasin <yasincaykur@gmail.com>
2022-04-21 16:38:42 +0300

    first set done

--- quiz-game/src/main.rs
+++ quiz-game/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::File;
+use std::io::{stdin, stdout, Write};
 use clap::Parser;
 use csv::{Reader, StringRecord};
 
@@ -8,30 +8,53 @@ struct Args {
     #[clap(short, long, default_value_t = 30)]
     limit: u32,
     #[clap(short, long, default_value = "problems.csv")]
-    filename: String
+    filename: String,
 }
+
 #[derive(Debug)]
-struct Problem {
-    question: *str,
-    answer: *str
+struct Question {
+    question: String,
+    answer: String,
 }
+
 
 fn main() {
     let args = Args::parse();
 
-    let mut reader = csv::Reader::from_path(args.filename)
-                                                .expect("a csv file");
+    let mut reader = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_path(args.filename)
+        .unwrap();
 
-    let mut problems: Vec<Problem>;
-    for result in reader.records() {
-        let record = result?;
-        let mut problem = Problem{
-            question: &record[0],
-            answer: &record[1]
-        };
-        println!("{:?}", problem);
-        problems.append(*problem)
+    let mut questions: Vec<Question> = vec![];
+
+    for record in reader.records() {
+        let mut question = Question{ question: String::new(), answer: String::new()};
+        match record.as_ref().unwrap().get(0) {
+            Some(value) => question.question = value.to_string(),
+            None => ()
+        }
+        match record.as_ref().unwrap().get(1) {
+            Some(value) => question.answer = value.to_string(),
+            None => ()
+        }
+        questions.push(question);
     }
 
-}
 
+    let mut total = 0;
+    for question in &questions {
+        let mut answer = String::new();
+        print!("{} = ", question.question);
+        stdout().flush();
+        stdin().read_line(&mut answer).unwrap();
+
+        if answer.trim() == question.answer {
+            total += 1;
+        }
+
+    }
+
+    println!("Correct Answer: {} Wrong Answer: {}", total, questions.len() - total);
+
+}
